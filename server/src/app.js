f5cb1f0e15439b7fd11fdfc975b9887ca76e7e87
Your Name <aayushman.shukla@gmail.com>
2026-05-31 10:44:58 +0530

    Fix: CORS and api url

--- server/src/app.js
+++ server/src/app.js
@@ -10,12 +10,11 @@ const allowedOrigins = [
 ]
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) callback(null, true)
-    else callback(new Error('Not allowed by CORS'))
-  },
-  credentials: true
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
+
 app.use(express.json());
 
 // Routes
